45d752097f49513009872c7b256f7ec3b535a9ef
aa3268 <aa3268@github.com>
2015-05-11 21:08:26 -0400

    Added project files and main window

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool running = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+
+
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+                time.Text = DateTime.Now.ToLongTimeString();
+                date.Text = DateTime.Now.ToLongDateString();
+        }
+
+        private void ButtonClickHandler(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            switch(button.Name)
+            {
+              /*
+               * Just type in the constructors for the pages 
+               * For example the code to create and display the main page is
+               * 
+               * new MainWindow().Show();
+               * 
+                case "Vitals": // Vitals page
+              
+                    break;
+                case "Settings": // Settings page
+                    
+                    break;
+                case "Medications": // Medications page
+                    
+                    break;
+               * 
+               */
+
+                case "Emergency":
+                    MessageBox.Show("Your emergency contact has been notified about your current condition.", "Emergency", MessageBoxButton.OK, MessageBoxImage.Warning);  
+                    break;
+
+            }
+
+        }
+    }
+}
